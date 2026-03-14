f45e8fadb83bf800ff055d8706936d92e1d8998b
ziomsy <pitervsa@gmail.com>
2026-03-14 12:01:59 +0100

    Final attempt to fix 404 assets with trailingSlash and GITHUB_ACTIONS check

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production' || process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  basePath: isProd ? '/portfolio' : '',
+  assetPrefix: isProd ? '/portfolio/' : '',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
