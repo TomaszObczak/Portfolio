129be466916012b7d3afd4c4a88fbbfe7be56bf1
ziomsy <pitervsa@gmail.com>
2026-03-14 11:59:36 +0100

    Disable Jekyll with .nojekyll and simplify config to fix 404 assets

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
   images: {
     unoptimized: true,
   },
